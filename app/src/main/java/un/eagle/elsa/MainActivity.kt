218e1d082533db010038318453eed0df2593230e
niquefaDiego <niquefa.diego@gmail.com>
2019-05-31 15:26:21 -0500

    add bottom navigation menu

--- app/src/main/java/un/eagle/elsa/MainActivity.kt
+++ app/src/main/java/un/eagle/elsa/MainActivity.kt
@@ -1,16 +1,37 @@
 package un.eagle.elsa
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import un.eagle.elsa.ui.login.LoginActivity
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textMessage: TextView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                textMessage.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_profile -> {
+                textMessage.setText(R.string.title_profile)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                textMessage.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var intent = Intent(this, LoginActivity::class.java);
-        startActivity(intent);
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        textMessage = findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
