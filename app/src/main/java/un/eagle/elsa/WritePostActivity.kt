6bd0fbd7770ddf6811a671601ad3360a2325e708
niquefaDiego <niquefa.diego@gmail.com>
2019-05-31 18:24:00 -0500

    add write post activity

---
+++ app/src/main/java/un/eagle/elsa/WritePostActivity.kt
@@ -0,0 +1,32 @@
+package un.eagle.elsa
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+
+class WritePostActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "Eagle.WritePostActivity"
+    }
+
+    lateinit var postEditText : EditText
+
+    private fun postIt() {
+        val post : String = postEditText.text.toString()
+        Log.d(TAG, "Post: \"$post\"");
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
+        setContentView(R.layout.activity_write_post)
+
+        val postButton : Button = findViewById(R.id.postButton)
+        postEditText = findViewById(R.id.postEditText)
+
+        postButton.setOnClickListener{ postIt() }
+    }
+}
--- app/src/main/java/un/eagle/elsa/fragments/HomeFragment.kt
+++ app/src/main/java/un/eagle/elsa/fragments/HomeFragment.kt
@@ -1,6 +1,8 @@
 package un.eagle.elsa.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -9,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import un.eagle.elsa.R
+import un.eagle.elsa.WritePostActivity
 import un.eagle.elsa.adapters.PostsAdapter
 import un.eagle.elsa.data.model.Post
 
@@ -28,16 +31,32 @@ class HomeFragment : Fragment() {
         return posts
     }
 
+
+    /**
+     * Goes to a write post activity
+     */
+    private fun goToWritePostActivity() {
+        activity?.let{
+            val intent = Intent (it, WritePostActivity ::class.java)
+            it.startActivity(intent)
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v : View  =  inflater.inflate(R.layout.fragment_home,null)
 
         val postsView : RecyclerView = v.findViewById(R.id.postsRecyclerView)
         postsView.layoutManager = LinearLayoutManager(context, LinearLayout.VERTICAL, false)
 
+        val writePostButton : FloatingActionButton = v.findViewById(R.id.writePostButton)
+
         val data = getSamplePosts()
 
         val postsAdapter = PostsAdapter(data)
         postsView.adapter = postsAdapter
+
+        writePostButton.setOnClickListener{ goToWritePostActivity() }
+
         return v
     }
 }
